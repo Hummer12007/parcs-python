f9129305c712c8a7c0d2b4896c0633c313b989de
Mykhailo Hodovaniuk <mykhailo.hodovaniuk@gmail.com>
2016-04-17 13:46:49 +0300

    ip config enh

--- parcs_py/parcs.py
+++ parcs_py/parcs.py
@@ -17,9 +17,9 @@ class Config:
     NODE_SECTION = 'Node'
     MASTER_NODE_SECTION = 'Master Node'
 
-    def __init__(self, port, master_ip=None, master_port=None):
+    def __init__(self, ip, port, master_ip=None, master_port=None):
         self.master = master_ip is None
-        self.ip = get_ip()
+        self.ip = ip if ip else get_ip()
         self.port = port if port else find_free_port()
         self.job_home = setup_working_directory()
         self.master_ip = master_ip
@@ -31,14 +31,16 @@ class Config:
         configuration.read(config_path)
 
         master = configuration.getboolean(Config.NODE_SECTION, 'master')
+        ip = configuration.get(Config.NODE_SECTION, 'ip') if configuration.has_option(Config.NODE_SECTION,
+                                                                                             'ip') else None
         port = configuration.getint(Config.NODE_SECTION, 'port') if configuration.has_option(Config.NODE_SECTION,
                                                                                              'port') else None
 
         if not master:
             master_ip = configuration.get(Config.MASTER_NODE_SECTION, 'ip')
             master_port = configuration.getint(Config.MASTER_NODE_SECTION, 'port')
-            return Config(port, master_ip, master_port)
-        return Config(port)
+            return Config(ip, port, master_ip, master_port)
+        return Config(ip, port)
 
 
 def start(conf):
--- start.py
+++ start.py
@@ -2,6 +2,7 @@ from parcs_py import parcs, Config
 import argparse
 
 parser = argparse.ArgumentParser(description='PARCS Python launcher...')
+parser.add_argument('-ip', help='Node ip')
 parser.add_argument('-port', help='Node port')
 parser.add_argument('-master_ip', help='Master node IP')
 parser.add_argument('-master_port', help='Master node port')
@@ -10,10 +11,11 @@ parser.add_argument('-config', help='Configuration file location')
 args = parser.parse_args()
 config = None
 if args.config is None:
+    ip = int(args.ip)
     port = int(args.port) if args.port else None
     master_ip = args.master_ip
     master_port = int(args.master_port) if args.master_port else None
-    config = Config(port, master_ip, master_port)
+    config = Config(ip, port, master_ip, master_port)
 else:
     config = Config.load_from_file(args.config)
 
